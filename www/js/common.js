63a7d42d385a184a9247a4a6e0140e7d519e4c64
MH <jpion.com@gmail.com>
2019-03-03 02:39:45 +0900

    수집하는 정보 안내 페이지 추가

--- www/js/common.js
+++ www/js/common.js
@@ -17,21 +17,23 @@ if ("geolocation" in navigator) {
     });
 }
 // 메뉴 버튼 제어
-$(document).ready(function(){
+$(document).ready(function () {
     // 헤더 부분 연결
     $('#header').load('header.html');
     // 푸터 부분 연결
     $('#footer').load('footer.html');
     // 메인 메뉴 열기 버튼
-    $('#btn_menu_open').click(function(){
+    $('#btn_menu_open').click(function () {
         $('#menu').css('display', 'block');
         $(this).css('display', 'none');
         $('#btn_menu_close').css('display', 'block');
     });
     // 메인 메뉴 닫기 버튼
-    $('#btn_menu_close').click(function(){
+    $('#btn_menu_close').click(function () {
         $('#menu').css('display', 'none');
         $(this).css('display', 'none');
         $('#btn_menu_open').css('display', 'block');
+        $('#menu_list, #btn-collect').show();
+        $('#collect-info, #btn-calculator').hide();
     });
 });
